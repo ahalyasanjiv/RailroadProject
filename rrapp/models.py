8b206cd734819df8ce4759c000f504aaa839b8fb
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2017-12-22 12:30:31 -0500

    Added some models

--- rrapp/models.py
+++ rrapp/models.py
@@ -3,3 +3,64 @@ from sqlalchemy.sql import func
 from sqlalchemy import desc
 
 db = SQLAlchemy()
+
+class FareType(db.Model):
+    """
+    Table that stores types of fares
+    """
+    __tablename__ = 'fare_types'
+    fare_id = db.Column(db.Integer, nullable=False, primary_key = True, autoincrement=True)
+    fare_name = db.Column(db.String(20))
+    rate = db.Column(db.Numeric(3,2))
+    
+
+    def __init__(self,judge_id,entry_id,vocals):
+        self.judge_id = judge_id
+        self.entry_id = entry_id
+        self.vocals = vocals
+
+class Passenger(db.Model):
+    """
+    Table that stores passengers
+    """
+    __tablename__ = 'passengers'
+    passenger_id = db.Column(db.Integer, nullable=False, primary_key = True, autoincrement=True)
+    fname = db.Column(db.String(30))
+    lname = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    preferred_card_number = db.Column(db.String(16))
+    preferred_billing_address = db.Column(db.String(100))
+    
+    
+
+    def __init__(self,fname,lname,email,password,preferred_card_number,preferred_billing_address):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.password = password
+        self.preferred_card_number = preferred_card_number
+        self.preferred_billing_address = preferred_billing_address
+
+
+class Reservation(db.Model):
+    """
+    Table that stores reservations
+    """
+    __tablename__ = 'reservations'
+    reservation_id = db.Column(db.Integer, nullable=False, primary_key = True, autoincrement=True)
+    reservation_date = db.Column(db.DateTime(timezone=False), default=func.now())
+    paying_passenger_id = db.Column(db.Integer, db.ForeignKey('passengers.passenger_id'), nullable=False)
+    card_number = db.Column(db.String(16))
+    billing_address = db.Column(db.String(100))
+    
+    
+
+    def __init__(self,reservation_date,paying_passenger_id,card_number,billing_address):
+        self.reservation_date = reservation_date
+        self.paying_passenger_id = paying_passenger_id
+        self.card_number = card_number
+        self.billing_address = billing_address
+
+
+        
