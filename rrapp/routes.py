eaf96c45f75dd802927cb691b47834c5a81dc9fc
Vincent Yu <vinjyu1@gmail.com>
2017-12-24 01:22:29 -0500

    modifying reservations, except nothing gets updated in db, also no seats free updating when modifying

--- rrapp/routes.py
+++ rrapp/routes.py
@@ -157,12 +157,80 @@ def viewReservations():
 def modifyReservation():
     if "user" not in session:
         return render_template("index.html")
+
+    modifyFee = 8
+    form = ReservationForm()
+
+    if request.method == "POST":
+        #Step 1: Choose Date and Stations Submitted
+        if session.get("choose_date_and_stations") and form.submit.data:
+            reservation_id_to_modify = session.get("reservation_id_to_modify", None)
+            reservation = Reservation.get_reservation_info(reservation_id_to_modify)
+            trip_info = Trips.get_trip_info_from_reservation_id(reservation_id_to_modify)
+            #session.pop("reservation_id_to_modify", None)
+            start = form.start_station.data
+            end = form.end_station.data
+            trip_date = form.date.data
+            start_station=int(start)
+            end_station=int(end)
+            available_trains = Trains.get_available_trains(start_station,end_station,trip_date)
+            session["start_station"] = start_station
+            session["end_station"] = end_station
+            session["trip_date"] = trip_date
+            session.pop("choose_date_and_stations", None)
+            session["choose_train"] = True
+            return render_template("modifyreservation.html", reservation=reservation, trip_info=trip_info, available_trains=available_trains, start_station=start_station, end_station=end_station, trip_date=trip_date)
+        #Step 2: Choose Train
+        elif session.get("choose_train") and request.form["train"] != "-1":
+            reservation_id_to_modify = session.get("reservation_id_to_modify", None)
+            reservation = Reservation.get_reservation_info(reservation_id_to_modify)
+            trip_info = Trips.get_trip_info_from_reservation_id(reservation_id_to_modify)
+            #session.pop("reservation_id_to_modify", None)
+            start_station = session.get("start_station", None)
+            session.pop("start_station", None)
+            end_station = session.get("end_station", None)
+            session.pop("end_station", None)
+            trip_date = session.get("trip_date", None)
+            session.pop("trip_date", None)
+            train_id = request.form["train"]
+            total_fare = float(Trips.get_trip_fare(start_station, end_station)) + modifyFee
+            time_in = str(Trains.get_train_time_in(train_id, start_station))
+            time_out = str(Trains.get_train_time_out(train_id, end_station))
+            new_trip_info = {"start_station":start_station, "end_station":end_station, "train_id":train_id, "trip_date":trip_date, "time_in":time_in, "time_out":time_out, "total_fare": total_fare}
+            session["new_trip_info"] = new_trip_info
+            session.pop("choose_train", None)
+            session["confirm_reservation"] = True
+            return render_template("modifyreservation.html", reservation=reservation, trip_info=trip_info, new_trip_info=new_trip_info, modifyFee=modifyFee)
+        #Step 3: Confirm Reservation
+        elif session.get("confirm_reservation") and request.form["submit"] == "Confirm Reservation":
+            reservation_id_to_modify = session.get("reservation_id_to_modify", None)
+            #reservation = Reservation.get_reservation_info(reservation_id_to_modify)
+            #trip_info = Trips.get_trip_info_from_reservation_id(reservation_id_to_modify)
+            #session.pop("reservation_id_to_modify", None)
+            start_station = session["new_trip_info"]["start_station"]
+            end_station = session["new_trip_info"]["end_station"]
+            train_id = session["new_trip_info"]["train_id"]
+            total_fare = session["new_trip_info"]["total_fare"]
+            trip_date = session["new_trip_info"]["trip_date"]
+            time_in = session["new_trip_info"]["time_in"]
+            time_out = session["new_trip_info"]["time_out"]
+            session.pop("new_trip_info", None)
+
+            db.session.query(Trips).filter_by(reservation_id=reservation_id_to_modify).update({"trip_date":trip_date, 
+                "trip_seg_start":start_station, "trip_seg_ends":end_station, "fare":total_fare, "trip_train_id":train_id})
+            session.pop("confirm_reservation", None)
+            return redirect(url_for("viewReservations"))
     else:
         reservation_id_to_modify = session.get("reservation_id_to_modify", None)
         reservation = Reservation.get_reservation_info(reservation_id_to_modify)
         trip_info = Trips.get_trip_info_from_reservation_id(reservation_id_to_modify)
-        session.pop("reservation_id_to_modify", None)
-        return render_template("modifyreservation.html", reservation=reservation, trip_info=trip_info)
+        #session.pop("reservation_id_to_modify", None)
+        #not popping because if a person refreshes the page we want to be able to have reservation_id_to_modify there
+        #if we popped, refreshing the page would raise an attribute error
+        session.pop("choose_train", None)
+        session.pop("confirm_reservation", None)
+        session["choose_date_and_stations"] = True
+        return render_template("modifyreservation.html", reservation=reservation, trip_info=trip_info, form=form)
 
 @app.route("/cancelreservation", methods=["GET", "POST"])
 def cancelReservation():
@@ -186,5 +254,7 @@ def cancelReservation():
         reservation_id_to_cancel = session.get("reservation_id_to_cancel", None)
         reservation = Reservation.get_reservation_info(reservation_id_to_cancel)
         trip_info = Trips.get_trip_info_from_reservation_id(reservation_id_to_cancel)
-        session.pop("reservation_id_to_cancel", None)
+        #session.pop("reservation_id_to_cancel", None)
+        #not popping because if a person refreshes the page we want to be able to have reservation_id_to_cancel there
+        #if we popped, refreshing the page would raise an attribute error
         return render_template("cancelreservation.html", reservation=reservation, trip_info=trip_info)
